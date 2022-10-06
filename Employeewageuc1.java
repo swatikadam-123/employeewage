69f8d27a41558be8eb62950c2e09776ca3aeb7a6
swatikadam-123 <swatikadam907@gmail.com>
2022-10-06 12:48:03 +0530

    Added: check attendence

---
+++ Employeewageuc1.java
@@ -0,0 +1,25 @@
+package Employeewage;
+
+
+public class Employeewageuc1 {
+
+	public static void main(String[] args) {
+		int is_fulltime=1;
+		double empcheck= Math.floor(Math.random()*10)%2;
+		
+		if(empcheck==is_fulltime)
+		{
+			System.out.println("employee is present");
+		}
+		else {
+			System.out.println("employee is absent");
+		}
+	
+	
+	
+	
+	}
+}
+			
+
+
